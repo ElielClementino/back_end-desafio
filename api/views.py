ea13752b3f74b88574ed2f342534ae75bd723425
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2022-09-21 10:53:56 -0300

    Adicionado logica da api

--- api/views.py
+++ api/views.py
@@ -4,10 +4,14 @@ from api.models import Tasks
 
 # Create your views here.
 def rendApi(requests):
-    title = Tasks.objects.all().values('title')
-    title_dict = [ {'title': item['title']} for item in title]
-    print(title_dict)
-    context = {
-        'tasks':title_dict,
-    }
-    return JsonResponse(context)
+    if requests.method == "POST":
+        post_data = requests.POST['title']
+        Tasks.save(post_data)
+    else:
+        title = Tasks.objects.all().values('title')
+        title_dict = [ {'title': item['title']} for item in title]
+        print(title_dict)
+        context = {
+            'tasks':title_dict,
+        }
+        return JsonResponse(context)
